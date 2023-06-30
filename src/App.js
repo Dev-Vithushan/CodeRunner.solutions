3d99149415459ebd4b8f275b7128e3f6f2982565
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2023-06-30 19:54:14 +0530

    New Branch created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import { Link } from 'react-router-dom';
 import Man from './images/man.jpg';
+import Hero from './section/Hero';
 
 function App() {
   return (
     <div >
       <Navbar />
-      <img  style={{ width: 550, height: 675, marginTop:30 }} src={Man} alt="Man" />
+      <Hero />
+     
   
     </div>
   );
